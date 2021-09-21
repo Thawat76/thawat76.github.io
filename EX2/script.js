f814c621c85e153d5a1d66b485a6760011f5845c
Thawat76 <deathenddragon@gmail.com>
2021-09-22 02:57:04 +0700

    test

---
+++ EX2/script.js
@@ -0,0 +1,6 @@
+let openLoginright = document.querySelector('.h1');
+let loginWrapper = document.querySelector('.login-wrapper');
+openLoginright.addEventListener('click',function()
+{
+    loginWrapper.classList.toggle('open');
+});
